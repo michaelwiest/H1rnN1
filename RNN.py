64a9670e435d058401b57467876dd6b13195a017
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-03-11 12:37:26 -0700

    first

--- RNN.py
+++ RNN.py
@@ -2,8 +2,6 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 import torch.nn.functional as F
-import numpy as np
-import torch.optim as optim
 
 class RNN(nn.Module):
     def __init__(self, input_size, num_filters, output_size,
@@ -13,12 +11,16 @@ class RNN(nn.Module):
         self.num_filters = num_filters
         self.output_size = output_size # Number of AAs
         self.n_layers = n_layers # Defaults to one.
+
+        if kernel_size % 2 != 0:
+            raise ValueError('Please supply an even number for kernel size')
         self.kernel_size = kernel_size
         self.lstm_hidden = lstm_hidden
         self.use_gpu = use_gpu
         self.batch_size = batch_size
 
-        self.c1 = nn.Conv1d(input_size, num_filters, kernel_size)
+        self.c1 = nn.Conv1d(input_size, num_filters, kernel_size,
+                            padding=int(self.kernel_size/2))
         # self.c2 = nn.Conv1d(hidden_size, hidden_size, 1)
         self.lstm = nn.LSTM(num_filters, lstm_hidden, n_layers, dropout=0.01)
         self.out = nn.Linear(lstm_hidden, output_size)
@@ -27,6 +29,8 @@ class RNN(nn.Module):
     def forward(self, inputs, hidden):
         batch_size = inputs.size(1)
         inputs = Variable(inputs.data.type(torch.FloatTensor))
+        # Turn (seq_len x batch_size x input_size) into (batch_size x input_size x seq_len) for CNN
+        # inputs = inputs.transpose(0, 1).transpose(1, 2)
 
         # Run through Conv1d and Pool1d layers
         c = self.c1(inputs)
@@ -56,101 +60,7 @@ class RNN(nn.Module):
     def init_hidden():
         self.__init_hidden()
 
-    def train(self, fasta_sampler, examples, seq_len, batch_size, epochs, lr):
-        ex_size = len(examples)
-        np.random.seed(1)
 
-        self.batch_size = batch_size
-        # slice data into trianing and testing (could do this much better)
-        val_split = 0.8
-        slice_ind = int((len(examples) * val_split))
-        training_data = examples[:slice_ind]
-        val_data = examples[slice_ind:]
-
-        if self.use_gpu:
-            self.cuda()
-
-        loss_function = nn.CrossEntropyLoss()
-        # Try Adagrad & RMSProp
-        optimizer = optim.SGD(self.parameters(), lr=lr)
-
-        # For logging the data for plotting
-        train_loss_vec = []
-        val_loss_vec = []
-
-        for epoch in range(epochs):
-            #get random slice
-            possible_example_indices = range(len(training_data))             #Idx of training examples
-            possible_slice_starts = [range(len(ex)) for ex in training_data] #len of each example
-            possible_val_indices = range(len(val_data))                      #Idx of val examples
-            # after going through all of a , will have gone through all possible 30
-            # character slices
-            iterate = 0
-
-            '''
-            Visit each possible example once. Can maybe tweak this to be more
-            stochastic.
-            '''
-            while len(possible_example_indices) > self.batch_size:
-                #Get #(batch_size) random training examples to take samples from
-                example_indices = random.sample(possible_example_indices, self.batch_size)
-
-                # Get processed data.
-                # print(len(possible_slice_starts[example_indices[0]]))
-                len_old = len(possible_example_indices)
-
-                rand_slice, targets = self.__convert_examples_to_targets_and_slices(training_data,
-                                                                                    example_indices,
-                                                                                    seq_len, ex_idx,
-                                                                                    center=False,
-                                                                                    possible_slice_starts=possible_slice_starts,
-                                                                                    possible_example_indices=possible_example_indices)
-
-                # print(len(possible_slice_starts[example_indices[0]]))
-                # if len_old != len(possible_example_indices):
-                #     print(len(possible_example_indices))
-                #     print('---')
-                # prepare data and targets for self
-                rand_slice = add_cuda_to_variable(rand_slice, self.use_gpu)
-                targets = add_cuda_to_variable(targets, self.use_gpu)
-
-                # Pytorch accumulates gradients. We need to clear them out before each instance
-                self.zero_grad()
-
-                # Also, we need to clear out the hidden state of the LSTM,
-                # detaching it from its history on the last instance.
-                self.__init_hidden()
-                # From TA:
-                # another option is to feed sequences sequentially and let hidden state continue
-                # could feed whole sequence, and then would kill hidden state
-
-                # Run our __forward pass.
-
-                outputs = self.__forward(rand_slice)
-                # Step 4. Compute the loss, gradients, and update the parameters by
-                #  calling optimizer.step()
-                loss = 0
-                for bat in range(batch_size):
-                    loss += loss_function(outputs[:,bat,:], targets[:,bat,:].squeeze(1))
-                loss.backward()
-                optimizer.step()
-
-                if iterate % 2000 == 0:
-                    print('Loss ' + str(loss.data[0] / self.batch_size))
-                    val_indices = random.sample(possible_val_indices, self.batch_size)
-                    val_inputs, val_targets = self.__convert_examples_to_targets_and_slices(val_data, val_indices, seq_len, ex_idx)
-
-                    val_inputs = add_cuda_to_variable(val_inputs, self.use_gpu)
-                    val_targets = add_cuda_to_variable(val_targets, self.use_gpu)
-                    self.__init_hidden()
-                    outputs_val = self.__forward(val_inputs)
-                    val_loss = 0
-                    for bat in range(self.batch_size):
-                        val_loss += loss_function(outputs_val[:,1,:], val_targets[:,1,:].squeeze(1))
-                    val_loss_vec.append(val_loss.data[0] / self.batch_size)
-                    train_loss_vec.append(loss.data[0] / self.batch_size)
-                    print('Validataion Loss ' + str(val_loss.data[0]/batch_size))
-                iterate += 1
-            print('Completed Epoch ' + str(epoch))
-
-        return train_loss_vec, val_loss_vec
+    def train(self, fasta_sampler, epochs):
+        for e in epochs:
+            ex = fasta_sampler.generate_N_sample(10, 2015)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-'''
 from fasta_sampler import *
 from RNN import *
 from helper import *
@@ -15,52 +14,10 @@ vocab = fs.vocabulary
 
 use_gpu = torch.cuda.is_available()
 ex = add_cuda_to_variable(ex, use_gpu)[0]
+# ex = ex.type(torch.FloatTensor)
+# inp = torch.randn(10, 1, 1702)
+# print(inp.size())
 
 rnn = RNN(1, 10, len(vocab.keys()), 30, 100, use_gpu, bs)
-rnn.forward(ex, rnn.hidden)
-'''
-
-from __future__ import print_function
-import torch.autograd as autograd
-from torch.autograd import Variable
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-#import matplotlib.pyplot as plt
-import random
-import pdb
-import numpy as np
-
-from helper import *
-from fasta_sampler import *
-from RNN import *
-
-fs = FastaSampler('data/HA_n_2010_2018.fa','data/HA_s_2010_2018.fa')
-
-batch_size = 10
-
-ex = fs.generate_N_sample(batch_size, 2013)
-vocab = fs.vocabulary
-input_size = 1
-
-# check for GPU
-use_gpu = torch.cuda.is_available()
-seq_len = 30
-hidden_layer_size = 100
-filter_num = 10
-kernal_size = 30
-
-batch_size = 10
-num_epochs = 10
-lr = 0.01
-
-model = RNN(input_size, filter_num, len(vocab.keys()), kernal_size, hidden_layer_size, use_gpu, batch_size)
-
-train_loss, val_loss = model.train(fs, ex, seq_len, batch_size,num_epochs, lr)
-
-#plt.plot(range(len(val_loss)), val_loss)
-#plt.plot(range(len(train_loss)), train_loss)
-#plt.show()
+o, h = rnn.forward(ex, rnn.hidden)
+print(o)
