b53d232aaea4e7dd17bc0ee0355e9e74c22591de
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-03-11 14:58:02 -0700

    added hidden vars to fxn call.

--- RNN.py
+++ RNN.py
@@ -120,7 +120,7 @@ class RNN(nn.Module):
                     targets = add_cuda_to_variable(targets, self.use_gpu)
 
                     self.__init_hidden()
-                    outputs_val = self.forward(val)
+                    outputs_val = self.forward(val, self.hidden)
                     outputs_val = outputs_val[1:, :, :]
                     targets = targets.transpose(0, 2).transpose(1, 2).long()
                     val_loss = 0
