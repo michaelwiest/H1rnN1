481b72654bc9dfe454cb0ebc3dc9497f593e56dd
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-03-11 14:21:45 -0700

    reshape data

--- RNN.py
+++ RNN.py
@@ -105,8 +105,9 @@ class RNN(nn.Module):
                 loss = 0
                 # print(outputs[1:, :, :].shape)
                 # print(targets.transpose(0,2).transpose(1,2).shape)
-                print(outputs)
-                print(targets)
+                outputs = outputs[1:, :, :]
+                targets = targets.transpose(0, 2).transpose(1, 2)
+                # print(targets)
                 for bat in range(batch_size):
                     loss += loss_function(outputs[1:, bat, :], targets[:, bat, :].squeeze(1))
                 loss.backward()
