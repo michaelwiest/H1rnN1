0d9587d8102b2ab14889d58a9ac60749ad9d8bb6
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-03-12 13:27:01 -0700

    call in correct place.

--- RNN.py
+++ RNN.py
@@ -145,11 +145,11 @@ class RNN(nn.Module):
 
         self.seq_len = len(primer_input)
         # build hidden layer
-        inp = add_cuda_to_variable(primer_input[:-1], self.use_gpu)
+        inp = add_cuda_to_variable(primer_input[:-1], self.use_gpu).unsqueeze(-1).transpose(0,2)
         print(inp)
         _ = self.forward(inp, self.hidden)
 
-        inp = add_cuda_to_variable(primer_input[:-1], self.use_gpu).unsqueeze(-1).transpose(0,2)
+        # inp = add_cuda_to_variable(primer_input[:-1], self.use_gpu).unsqueeze(-1).transpose(0,2)
         self.seq_len = 1
         predicted = list(primer_input)
         if predict_len is not None:
