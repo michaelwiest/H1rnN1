32dae09ae0c9f1b0e6909d6f07d472ab95a5f982
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-03-15 10:14:01 -0700

    daydream index fixing

--- RNN.py
+++ RNN.py
@@ -88,6 +88,8 @@ class RNN(nn.Module):
         outs = [conv(inputs)[:, :, :num_targets] for conv in self.convs]
         if chomp_len is not None:
             inputs = inputs[:, :, (self.first_kernel_size):]
+        else:
+            inputs = inputs[:, :, 1:]
         for out in outs:
             print(out.size())
         print(inputs.size())
