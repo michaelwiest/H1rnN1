4787e7c224d8b102ac3b791a59277e7f0482b8e4
Michael Wiest <michaelewiest@gmail.com>
2018-03-19 21:26:44 -0700

    Code works now

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,7 @@
 from fasta_sampler_v2 import *
 from RNN_v2 import *
-from scipy.spatial.distance import *
+import scipy
+
 
 class Evaluator(object):
     def __init__(self, fasta_sampler, model):
@@ -9,6 +10,10 @@ class Evaluator(object):
 
 
     def get_dist_matrix(self, seqs_a, seqs_b):
+        if len(seqs_a.shape) == 1:
+            seqs_a = np.array([seqs_a]).T
+        if len(seqs_b.shape) == 1:
+            seqs_b = np.array([seqs_b]).T
         seqs_a = np.vectorize(self.fs.vocabulary.get)(seqs_a)
         seqs_b = np.vectorize(self.fs.vocabulary.get)(seqs_b)
         return scipy.spatial.distance.cdist(seqs_a, seqs_b, metric='hamming')
@@ -18,5 +23,6 @@ class Evaluator(object):
         predictions = self.model.batch_dream(num_samples, primer, year, temp,
                                              self.fs, predict_len)
         df = self.fs.get_dataframe()
-        actuals = df[df['year'] == year].sample(num_samples)['seq'].values
+        actuals = df[df['year'] == year + 1].sample(num_samples)['seq_list'].values
+        actuals = np.array(actuals.tolist())
         return self.get_dist_matrix(predictions, actuals)
